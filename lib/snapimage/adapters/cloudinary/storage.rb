e1a86e753154586a33a67ddef24e91d697c09437
Wesley Wong <wesley@snapeditor.com>
2013-09-06 10:49:17 -0700

    Cloudinary uses the directory option if available.

--- lib/snapimage/adapters/cloudinary/storage.rb
+++ lib/snapimage/adapters/cloudinary/storage.rb
@@ -7,11 +7,13 @@ module SnapImage
 
       # Stores the file in the given directory and returns the publicly
       # accessible URL.
-      # Options: none
+      # Options:
+      # * directory - directory to store the file in
       def store(filename, file, options = {})
         ext = File.extname(filename)
         basename = File.basename(filename, ext)
         public_id = "#{basename}_#{rand(9999)}"
+        public_id = File.join(options[:directory], public_id) if options[:directory]
         response = ::Cloudinary::Uploader.upload(file, public_id: public_id)
         ::Cloudinary::Utils.cloudinary_url(public_id + ext)
       end
